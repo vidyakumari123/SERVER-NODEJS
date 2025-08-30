169ce846585070cfc3d1ac1575b001b1cfd2eab8
vidyakumari123 <127233800+vidyakumari123@users.noreply.github.com>
2025-08-30 20:12:15 +0530

    HANDLING URL

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const http = require("http")
 
 const fs =require ("fs")
+const url = require("url")
 const myServer = http.createServer((req,res)=>{
+    if(req.url === "/favicon.ico")return res.end()
  const log =`${Date.now()}:${req.url}New Req Received\n`
+const myUrl= url.parse(req.url,true)
+console.log(myUrl)
     fs.appendFile('log.txt',log ,(err, data)=>{
-        switch (req.url){
+        switch (myUrl.pathname){
             case '/':
                  res.end ("Homepage")
             break
             case '/about':
-                 res.end ("I am vidya")
+                const username = myUrl.query.myname 
+                res.end(`Hi,${username}`)
+                 res.end ("I am vidya hello")
             break
             default:
                 res.end("404 Not found")
